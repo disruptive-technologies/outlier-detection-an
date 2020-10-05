514100b05c289add1f719ab4d9b19c568686af0a
jhgjeraker <jhgjeraker@protonmail.com>
2020-10-05 10:10:50 +0200

    reset auth details

--- sensor_stream.py
+++ sensor_stream.py
@@ -2,9 +2,9 @@
 from outlier.director import Director
 
 # Fill in from the Service Account and Project:
-USERNAME   = 'bsvsebr24spg00b250ag'       # this is the key
-PASSWORD   = '09e436a4ab3e44779b36ded5f5ff1386'     # this is the secret
-PROJECT_ID = 'br793014jplfqcpoj45g'                # this is the project id
+USERNAME   = "SERVICE_ACCOUNT_KEY"       # this is the key
+PASSWORD   = "SERVICE_ACCOUT_SECRET"     # this is the secret
+PROJECT_ID = "PROJECT_ID"                # this is the project id
 
 # url base and endpoint
 API_URL_BASE  = 'https://api.disruptive-technologies.com/v2'
